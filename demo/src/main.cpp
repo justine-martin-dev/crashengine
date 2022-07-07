d685a742fa46260b5ef18eedff0496e657acfc51
Justine Martin <justine.martin.dev@gmail.com>
2022-07-08 01:24:10 +0200

    feat : Initial commit

---
+++ demo/src/main.cpp
@@ -0,0 +1,7 @@
+#include <crashengine/main.hpp>
+
+int main(int argc, char **argv) {
+	test();
+
+	return 0;
+}
---
+++ src/crashengine/main.cpp
@@ -0,0 +1,5 @@
+#include "crashengine/main.hpp"
+
+void test() {
+	std::cout << "Hello qsdklmcvkjnnnhlu.... crash engine :)" << std::endl;
+}
